26ba45be3ce4056486f508bda7b293c35fad37e8
Randell Miller <theubie@gmail.com>
2025-06-06 01:05:11 -0500

    Add localStorage persistence for generator options

--- track-generator/js/generator.js
+++ track-generator/js/generator.js
@@ -30,6 +30,35 @@
         }
     };
 
+    $(function() {
+        if (!window.localStorage) { return; }
+        var stored = localStorage.getItem('tg-options');
+        if (!stored) { return; }
+        try {
+            var opts = JSON.parse(stored);
+            if (opts.bpmMin !== undefined) {
+                $('#tg-bpm-min').val(opts.bpmMin);
+            }
+            if (opts.bpmMax !== undefined) {
+                $('#tg-bpm-max').val(opts.bpmMax);
+            }
+            if (Array.isArray(opts.modes)) {
+                $('input[name="tg-modes[]"]').prop('checked', false).each(function(){
+                    if (opts.modes.indexOf($(this).val()) !== -1) {
+                        $(this).prop('checked', true);
+                    }
+                });
+            }
+            if (opts.progType) {
+                $('input[name="tg-prog-type"][value="' + opts.progType + '"]')
+                    .prop('checked', true);
+            }
+            if (opts.progLength !== undefined) {
+                $('#tg-prog-length').val(opts.progLength);
+            }
+        } catch(e) {}
+    });
+
     $(document).on('click', '#tg-generate', function() {
         var bpmMin = parseInt($('#tg-bpm-min').val(), 10);
         var bpmMax = parseInt($('#tg-bpm-max').val(), 10);
@@ -39,6 +68,17 @@
         });
         var progType = $('input[name="tg-prog-type"]:checked').val();
         var progLength = parseInt($('#tg-prog-length').val(), 10) || 4;
+
+        if (window.localStorage) {
+            var opts = {
+                bpmMin: bpmMin,
+                bpmMax: bpmMax,
+                modes: modes,
+                progType: progType,
+                progLength: progLength
+            };
+            localStorage.setItem('tg-options', JSON.stringify(opts));
+        }
 
         var result = '';
         result += '<p><strong>BPM:</strong> ' + tg.generateBPM(bpmMin, bpmMax) + '</p>';
